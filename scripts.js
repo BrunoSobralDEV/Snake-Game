a67dbc92606c541a42506e912c46491fa0d19009
BrunoSobralDEV <brunosobralss@hotmail.com>
2021-09-02 14:15:19 -0300

    adicionado alert end game

--- scripts.js
+++ scripts.js
@@ -48,12 +48,20 @@ function update(event) { //37=direita,38=pra baixo
 //passar todas as funções pra quando iniciar, criar o background e criar a cobrinha
 function iniciarJogo() {
     //atravessar a parede - voltar pro início
-    //plano cartesiano
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    //gameOver
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            //parar jogo
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+    
     criarBG();
     criarCobrinha();  
     drawFood();
